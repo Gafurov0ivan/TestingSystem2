0ff8da58ebee596b3212f35fc70ee186eb2a65b8
k.iskhakova <k.iskhakova@iis-soft.ru>
2016-11-08 17:42:30 +0300

    #9 UserDao (Impl)

--- src/main/java/ru/itpark/dao/UserDao.java
+++ src/main/java/ru/itpark/dao/UserDao.java
@@ -1,10 +1,5 @@
 package ru.itpark.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ru.itpark.entity.User;
 
 import java.util.List;
@@ -13,23 +8,8 @@ import java.util.List;
  * @author Kamila Iskhakova
  *         Created on 07.11.2016
  */
-@Repository
-public class UserDao {
 
-  @Autowired
-  private SessionFactory sessionFactory;
+public interface UserDao<E> extends BaseDao<E> {
 
-  @Transactional
-  public List<User> getAllUsers(){
-    return getSession().createCriteria(User.class).list();
-  }
-
-  @Transactional
-  public void save(User user) {
-    getSession().saveOrUpdate(user);
-  }
-
-  private Session getSession() {
-    return sessionFactory.getCurrentSession();
-  }
+  List<User> getAllUsers();
 }
--- src/main/java/ru/itpark/impl/BaseDaoImpl.java
+++ src/main/java/ru/itpark/impl/BaseDaoImpl.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
 import ru.itpark.dao.BaseDao;
 import ru.itpark.entity.BaseEntity;
 
@@ -34,12 +35,12 @@ public class BaseDaoImpl<E extends BaseEntity> implements BaseDao<E> {
   }
 
 
+  protected Criteria getCriteria() {
+    return getSession().createCriteria(entityClass);
+  }
+
   @Transactional
   public void saveOrUpdate(E entity) {
     getSession().saveOrUpdate(entity);
-  }
-
-  protected Criteria getCriteria() {
-    return getSession().createCriteria(entityClass);
   }
 }
