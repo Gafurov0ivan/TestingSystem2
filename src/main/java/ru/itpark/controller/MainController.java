6d779733d6c5cdf971f4ff0e038f3aa8524d6628
k.iskhakova <k.iskhakova@iis-soft.ru>
2016-11-08 13:01:14 +0300

    #4 mvc-dispatcher, web.xml

--- src/main/java/ru/itpark/controller/MainController.java
+++ src/main/java/ru/itpark/controller/MainController.java
@@ -1,13 +1,7 @@
 package ru.itpark.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import ru.itpark.dao.UserDao;
-import ru.itpark.entity.User;
-
-import java.util.List;
 
 /**
  * @author Kamila Iskhakova
@@ -16,18 +10,8 @@ import java.util.List;
 @Controller
 public class MainController {
 
-  @Autowired
-  private UserDao userDao;
-
-  @RequestMapping(value = "/hello")
-  public ModelAndView showHelloPage() {
-    return new ModelAndView("hello");
-  }
 
   @RequestMapping(value = "/")
-  public ModelAndView showStart() {
-    //List<User> users = userDao.getAllUsers();
-    //userDao.save(new User(1L, "Kamila"));
-    return new ModelAndView("hello");
+  public void showStart() {
   }
 }
